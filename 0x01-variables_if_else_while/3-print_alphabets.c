a845754e9962363010fb523bdabdbaecaa9b05c1
Donatussss <don.sp4r@gmail.com>
2023-09-09 01:12:25 +0300

    0x01 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - main block
+*
+* Description - print alphabet
+*
+* Return: 0
+*/
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 'A';
+
+	while (i <= 'Z')
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
