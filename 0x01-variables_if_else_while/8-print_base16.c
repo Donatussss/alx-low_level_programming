6e361fb7e4668f1d6fb0c36bc93cf97cf60c2731
Donatussss <don.sp4r@gmail.com>
2023-09-09 01:33:43 +0300

    0x01 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - main block
+*
+* Description - print alphabet
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 65
+	
+	while (i < 70)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
