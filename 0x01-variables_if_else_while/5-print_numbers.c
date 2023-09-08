3a8245324fef573424328a6e0028066603174be1
Donatussss <don.sp4r@gmail.com>
2023-09-09 01:24:04 +0300

    0x01 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - main block
+*
+* Description - print alphabet
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
