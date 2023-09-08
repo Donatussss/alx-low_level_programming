14ac6d5bd42d8a535eb09002f3472d4975954019
Donatussss <don.sp4r@gmail.com>
2023-09-09 02:54:38 +0300

    0x01 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - main block
+*
+* Description - print alphabet
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48
+
+	while (i < 56)
+	{
+		j = 48;
+		while (j < 57)
+		{
+			k = 48;
+			while (k < 58){
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if ((i + j) !=  167)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
