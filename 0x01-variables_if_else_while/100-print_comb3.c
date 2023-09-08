ea7eddc54ddda1fd47d8cd5cbbda6e8e3f0aa88c
Donatussss <don.sp4r@gmail.com>
2023-09-09 02:27:54 +0300

    0x01 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - main block
+*
+* Description - print alphabet
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+	int j = 0;
+	int modval = 48;
+
+	while (i < 58)
+	{
+		j = 0;
+
+		while (j < 2)
+		{
+			if ((i + j) > 57)
+			{
+				modval = 58;
+			}
+			else
+			{
+				modval = 48;
+			}
+			putchar(i);
+			putchar(48 + ((i + j) % modval));
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
