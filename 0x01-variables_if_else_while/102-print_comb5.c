614ac080d273a11e6027282025fbd6902a855cfa
Donatussss <don.sp4r@gmail.com>
2023-09-09 03:37:00 +0300

    0x01 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Description - Combination of four numbers with space
+* Return: 0 success
+*/
+int main(void)
+{
+	int count1 = 0;
+	int digit1, digit2;
+	int count2;
+	int digit3, digit4;
+	while (num1 <= 98)
+	{
+		digit1  = (count1 / 10 + 48);
+		digit2 = (count1 % 10 + 48);
+		count2 = 0;
+		while (count2 <= 99)
+		{
+			digit3 = (count2 / 10 + 48);
+			digit4 = (count2 % 10 + 48);
+			if (count1 < count2)
+			{
+				putchar(digit1);
+				putchar(digigit2);
+				putchar(' ');
+				putchar(digit3);
+				putchar(digit4);
+				if (num1 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}}
+				count2++;
+			}
+			count1++;
+		}
+		putchar('\n');
+	return (0);
+}
