358aa37137a010177703d01ce03e6ac244a901f4
Donatussss <don.sp4r@gmail.com>
2023-09-13 02:58:58 +0300

    0x02 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - working func
+ * Description - prints the 1st 50 fibonacci numbers
+ * Return: no return data
+ */
+void fibonacci(void)
+{
+	int i = 0;
+	int j = 0;
+	int sum = 1;
+	int fib[51];
+	fib[0] = 0;
+	fib[1] = 1;
+
+	while (i <= 50)
+	{
+		j = 0;
+		sum = 0;
+		while (j < i)
+		{
+			sum += fib[j];
+			j++;
+		}
+
+		if(sum > 0)
+		{
+			fib[i] = sum;
+			print("%d", sum);
+		}
+
+		if (i < 50)
+		{
+			print(", ");
+		}
+
+		i++;
+	}
+
+	printf("%d\n", sum);
+}
+
+/**
+ * main - entry point
+ * Description - executes natural
+ * Return: no return data
+ */
+int main(void)
+{
+	natural();
+	return (0);
+}
