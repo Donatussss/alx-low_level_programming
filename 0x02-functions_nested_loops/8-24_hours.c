d948977bf5e3273ec6f408969b0d578536f7695d
Donatussss <don.sp4r@gmail.com>
2023-09-13 00:14:36 +0300

    0x02 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point
+ * Description - prints every minute of the day
+ * Return: no return data
+ */
+
+int jack_bauer(void)
+{
+	int n = 0;
+	int x = 0;
+
+	while (n < 24)
+	{
+		x = 0
+		while (x < 60)
+		{
+			if (n < 10)
+			{
+				_putchar('0');
+			}
+
+			_putchar(n + '0');
+			_putchar(':');
+
+			if (x < 10)
+			{
+				_putchar('0');
+			}
+
+			_putchar(x + '0');
+			_putchar('\n');
+			x++;
+		}
+		
+		n++;
+	}
+}
