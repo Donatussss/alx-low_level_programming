1efdecad2b31e29d4d0a2222546716d8294e3aed
Donatussss <don.sp4r@gmail.com>
2023-09-12 23:53:50 +0300

    0x02 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * Description - prints last digit of number
+ * @n : number to print last digit
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	return (n%10);
+}
