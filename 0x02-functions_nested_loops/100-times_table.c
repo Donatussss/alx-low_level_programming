7330693b379c6bed1cf5f9cd891bae4d1bd8bb6f
Donatussss <don.sp4r@gmail.com>
2023-09-13 02:15:08 +0300

    0x02 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,32 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * print_first_digit - used in jack_bauer
- * Description - prints first digit of number with 2 or more digits
- * @x: int with 2 or more digits
- * Return: no return data
- */
-void print_first_digit(int x)
-{
-	int x1 = 1;
-	int diffx = x - 10;
-
-	while (diffx >= 10)
-	{
-		x1++;
-		diffx -= 10;
-	}
-
-	if (x1 < 10)
-	{
-		_putchar(x1 + '0');
-	}
-	else if (x >= 10)
-	{
-		print_first_digit(x1);
-		_putchar((x1 % 10) + '0');
-	}
-}
-
 /**
  * print_times_table - entry point
  * Description - prints n times table
@@ -48,32 +21,23 @@ void print_times_table(int n)
 			r = i * j;
 			if (j > 0 && r < 10)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
+				printf(",   %d", r);
 			}
 
 			else if (r >= 10 && r < 100)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				print_first_digit(r);
+				printf(",  %d", r);
 			}
 
 			else if (r >= 100)
 			{
-				_putchar(',');
-				_putchar(' ');
-				print_first_digit(r);
+				printf(", %d", r);
 			}
 
-			_putchar((r % 10) + '0');
 			j++;
 
 		}
-		_putchar('\n');
+		printf("\n");
 		i++;
 	}
 }
