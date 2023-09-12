1c81ccd8dea99d45237b96e936265bfc31bbf3f1
Donatussss <don.sp4r@gmail.com>
2023-09-13 02:30:39 +0300

    0x02 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * natural - entry point
+ * Description - prints the sum of multiples of 3 or 5 below 1024
+ * Return: no return data
+ */
+
+void natural(void)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+
+		i++;
+	}
+
+	print("%d\n", sum);
+}
