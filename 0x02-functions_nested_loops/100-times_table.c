73984178bcd7bdcbc152db9095f92b769612f0ca
Donatussss <don.sp4r@gmail.com>
2023-09-13 01:45:28 +0300

    0x02 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,19 +27,15 @@ void print_first_digit(int x)
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
-
 	int i = 0;
 	int j = 0;
 	int r = 0;
+	int n1 = n ? n <= 12 : 0;
 
-	while (i <= n)
+	while (i <= n1)
 	{
 		j = 0;
-		while (j <= n)
+		while (j <= n1)
 		{
 			r = i * j;
 			if (j > 0 && r < 10)
