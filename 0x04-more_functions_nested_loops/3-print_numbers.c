fc25743da3aa60e3d4a32cd7be425be67f31c580
Donatussss <don.sp4r@gmail.com>
2023-09-14 21:34:59 +0300

    0x04 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * Return: no return data
+ */
+
+void print_numbers(void)
+{
+	int x = 48;
+	
+	while (x <= 57)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
