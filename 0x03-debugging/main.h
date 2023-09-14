173ddbbfe82c0d153258ce732b88e26c0bdfb3fd
Donatussss <don.sp4r@gmail.com>
2023-09-14 18:54:09 +0300

    0x03 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 #endif
