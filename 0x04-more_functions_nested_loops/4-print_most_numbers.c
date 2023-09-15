3ade087de494a284644b68c8f723bf777fbf5058
Donatussss <don.sp4r@gmail.com>
2023-09-15 03:40:11 +0300

    0x04 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * Description - print 0 to 9
+ * Return: no return data
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+	
+	while (x <= 9 && x != 2 && x != 4)
+	{
+		_putchar(x + '0');
+		x++;
+	}
+	_putchar('\n');
+}
