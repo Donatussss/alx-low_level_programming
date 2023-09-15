de173dbd083260119dd65e66a17ae274ca854ea4
Donatussss <don.sp4r@gmail.com>
2023-09-15 04:13:03 +0300

    0x04 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,12 @@
 
 void print_most_numbers(void)
 {
-	int x = 0;
-	
-	while (x <= 9)
+	for (int x = 0; x <= 9: x++)
 	{
 		if (x != 2 && x != 4)
 		{
 			_putchar(x + '0');
 		}
-		x++;
 	}
 	_putchar('\n');
 }
