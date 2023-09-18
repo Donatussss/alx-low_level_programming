38afe587d7d2ccdaced46b7fd2ffed0b512f5a9e
Donatussss <don.sp4r@gmail.com>
2023-09-18 22:44:14 +0300

    0x05 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: pointer to first char in string
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+	int str_len = 0;
+	char comp;
+
+	while (comp != 0)
+	{
+		str_len++;
+		comp = *(s + str_len);
+	}
+
+	return str_len;
+}
