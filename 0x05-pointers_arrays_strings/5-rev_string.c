ce99056a956d6b32405396056ac22aa41fb1ab86
Donatussss <don.sp4r@gmail.com>
2023-09-19 00:07:57 +0300

    0x05 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: pointer to first char in string
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+	int str_len = 0;
+	char comp = *s;
+
+	while (comp != '\0')
+	{
+		str_len++;
+		comp = *(s + str_len);
+	}
+
+	return (str_len);
+}
+
+/**
+ * rev-string - prints a string, in reverse
+ * @s: address of first character
+ * Return: no return data
+ */
+
+void rev_string(char *s)
+{
+	int rise = 0;
+	int len = _strlen(s) - 1;
+	char temp = *s;
+
+	while (len != rise)
+	{
+		temp = *(s + len);
+		*(s + len) = *(s + rise);
+		*(s + rise) = temp;
+		rise++;
+		len--;
+	}
+}
