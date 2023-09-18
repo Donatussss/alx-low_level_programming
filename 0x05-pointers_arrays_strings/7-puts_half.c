55f01d9bd5359708bfeadf15378af614e14735f5
Donatussss <don.sp4r@gmail.com>
2023-09-19 00:48:34 +0300

    0x05 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: pointer to first char in string
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+	int str_len = 0;
+	char comp = *s;
+
+	while (comp != '\0')
+	{
+		str_len++;
+		comp = *(s + str_len);
+	}
+
+	return (str_len);
+}
+
+/**
+ * puts_half - prints half of a string,
+ * followed by a new line
+ * @str: pointer to first char in string
+ * Return: no return data
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int cond = len % 2 != 0 ? 1 : 0;
+	int n = (len - cond) / 2;
+	int count = 0;
+
+	while (count < len)
+	{
+		if (count >= n)
+		{
+			_putchar(*(str + count));
+		}
+
+		count++;
+	}
+
+	_putchar('\n');
+}
