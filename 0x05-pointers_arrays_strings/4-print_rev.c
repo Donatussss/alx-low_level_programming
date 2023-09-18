f201dae8d1e5536d3d4b018de080f3d74b0a7e36
Donatussss <don.sp4r@gmail.com>
2023-09-18 23:21:59 +0300

    0x05 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - returns length of string
+ * @s: pointer to first char in string
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+        int str_len = 0;
+        char comp = *s;
+
+        while (comp != '\0')
+        {
+                str_len++;
+                comp = *(s + str_len);
+        }
+
+        return (str_len);
+}
+
+/**
  * print_rev - prints a string, in reverse,
  * followed by a new line
  * @s: address of first character
