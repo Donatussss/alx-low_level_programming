265cf61c4d243ed8204c6c207792d5cd4b149bfd
Donatussss <don.sp4r@gmail.com>
2023-09-22 07:34:20 +0300

    0x06 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
-* rot13 - encodes a string using rot13
-* @s: pointer to string to encode
-* Return: pointer to encoded string
-*/
+ * rot13 - encodes string in rot13
+ * @s: pointer to string
+ * Return: pointer to string
+ */
 
 char *rot13(char *s)
 {
 	int i = 0;
-	int j;
-	char alphbt[] = "abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
+	
 	while (*(s + i) != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		if (*(s + i) >= 97 && *(s + i) <= 122)
 		{
+			*(s + i) = ((*(s + i) - 97) + 13) % 26 + 97;
+		}
 
-			if (*(s + i) == alphbt[j])
-			{
-				(*(s + i) = rot13[j]);
-				break;
-			}
+		else if (*(s + i) >= 65 && *(s + i) <= 90)
+		{
+			*(s + i) = ((*(s + i) - 65) + 13) % 26 + 65;
 		}
 
 		i++;
