84c92ae9c8c447736f00ab94f1136f4605c44a28
Donatussss <don.sp4r@gmail.com>
2023-10-03 18:02:07 +0300

    0x0B 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr1 = (int **)malloc(sizeof(int *) * width);
+	arr1 = (int **)malloc(sizeof(int *) * height);
 
 	if (arr1 == NULL)
 	{
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 
 	while (i < width)
 	{
-		*(arr1 + i) = (int *)calloc(height, sizeof(int));
+		*(arr1 + i) = (int *)calloc(width, sizeof(int));
 
 		if (*(arr1 + i) == NULL)
 		{
