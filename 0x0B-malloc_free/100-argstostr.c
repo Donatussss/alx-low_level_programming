5a7d7bb6fe4e51fe6e99d96e90a5c36cc11f950e
Donatussss <don.sp4r@gmail.com>
2023-10-03 22:19:21 +0300

    0x0B 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: pointer to first char in string
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+	int str_len = 0;
+	char comp = *s;
+
+	while (comp != '\0')
+	{
+		str_len++;
+		comp = *(s + str_len);
+	}
+
+	return (str_len);
+}
+
+/**
+ * argstostr - a function that concatenates all the arguments
+ * of your program
+ * Each argument should be followed by a \n in the new string
+ * @ac: argument count
+ * @av: pointer to array of 'pointers of argument strings'
+ * Return: a pointer to a new string, or NULL if it fails
+ * NULL if ac == 0 or av == NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, k = 0;
+	char *new_str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (i < ac)
+	{
+		j += _strlen(*(av + i));
+		i++;
+	}
+
+	new_str = malloc(sizeof(char) * (j + 1));
+	i = 0;
+
+	if (new_str == NULL)
+	{
+		free(new_str);
+		return (NULL);
+	}
+
+	while (i < ac)
+	{
+		j = 0;
+
+		while (j < _strlen(*(av + i)))
+		{
+			*(new_str + k) = *(*(av + i) + j);
+			j++;
+			k++;
+		}
+
+		*(new_str + k) = '\n';
+		k++;
+		i++;
+	}
+
+	return (new_str);
+}
