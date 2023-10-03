fdc24d5c1645e29dd898b716577c006698df7755
Donatussss <don.sp4r@gmail.com>
2023-10-03 18:31:03 +0300

    0x0B 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,71 +1,38 @@
-#include "main.h"
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * _strlen - returns length of string
- * @s: pointer to first char in string
- * Return: int length of string
- */
-
-int _strlen(char *s)
-{
-	unsigned int str_len = 0;
-	char comp = *s;
-
-	while (comp != '\0')
-	{
-		str_len++;
-		comp = *(s + str_len);
-	}
-
-	return (str_len);
-}
-
-/**
- * str_concat - a function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer should point to a newly allocated space in memory
- * which contains the contents of s1, followed by the contents of s2
- * and null terminated
- * if NULL is passed, treat it as an empty string
- * Return NULL on failure
- */
-
+* str_concat - concatenates two strings
+* @s1: string 1
+* @s2: string 2
+* Return: NULL or concatenated string
+*/
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i = 0, j = 0;
-	unsigned int str_len1, str_len2 = 0;
-	char *new_str;
+	char *word;
+	unsigned int num1, num2, num3, max;
 
-	if (s1 != NULL)
-		str_len1 = _strlen(s1);
+	if (s1 == NULL)
+		s1 = "";/* null string*/
+	if (s2 == NULL)
+		s2 = "";
 
-	if (s2 != NULL)
-		str_len2 = _strlen(s2);
+	for (num1 = 0; s1[num1] != '\0'; num1++)
+	;
+	for (num2 = 0; s2[num2] != '\0'; num2++)
+	;
 
-	new_str = malloc(sizeof(char) * (str_len1 + str_len2 + 1));
+	word = malloc(sizeof(char) * (num1 + num2 + 1));
 
-	if (new_str == NULL)
+	if (word == NULL)
 	{
-		free(new_str);
+		free(word);
 		return (NULL);
 	}
+	for (num3 = 0; num3 < num1; num3++)
+		word[num3] = s1[num3];
+	max = num2;
 
-	while (i < str_len1)
-	{
-		*(new_str + i) = *(s1 + i);
-		i++;
-	}
-
-	while (j < str_len2)
-	{
-		*(new_str + i) = *(s2 + j);
-		i++;
-		j++;
-	}
-
-	 *(new_str + i) = '\0';
-
-	return (new_str);
+	for (num2 = 0; num2 <= max; num3++, num2++)
+		word[num3] = s2[num2];
+	return (word);
 }
