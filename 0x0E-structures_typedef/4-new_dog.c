5b8df8b72076dafeec43711d2f239c61f8e9f3bc
Donatussss <don.sp4r@gmail.com>
2023-10-09 23:35:54 +0300

    0x0E 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,11 +6,13 @@
  * @name: dog name
  * @age: dog age
  * @owner: owner name
+ * Return: pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
+
 	dog1 = malloc(sizeof(dog_t));
 
 	if (dog1 == NULL)
