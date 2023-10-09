afc3d0490a3ab5d3e7492d27b22fa87610182745
Donatussss <don.sp4r@gmail.com>
2023-10-09 22:52:52 +0300

    0x0E 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HIHIHI
+#define HIHIHI
+
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
