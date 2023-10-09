7cf4b45df5cdc20553b202e76646af0b62987806
Donatussss <don.sp4r@gmail.com>
2023-10-09 21:41:14 +0300

    0x0D 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file
+ * it was compiled from, followed by a new line
+ * Return: 0
+ */
+
+int main()
+{
+	printf(__FILE__"\n");
+	return (0);
+}
