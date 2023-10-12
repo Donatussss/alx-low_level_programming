0d22220c5f891f31d81b5d12b710c1c92030f90c
Donatussss <don.sp4r@gmail.com>
2023-10-13 02:58:01 +0300

    0x10 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -57,13 +57,6 @@ void print_s(va_list ap)
  * f: float
  * s: char * (if the string is NULL, print (nil) instead)
  * any other char should be ignored
- * You are not allowed to use for, goto, ternary operator, else, do ... while
- * You can use a maximum of
- * 2 while loops
- * 2 if
- * You can declare a maximum of 9 variables
- * You are allowed to use printf
- * Print a new line at the end of your function
  */
 
 void print_all(const char * const format, ...)
