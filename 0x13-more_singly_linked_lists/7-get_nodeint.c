20d330ba927ca1b00af726049e2e89c566af0335
Donatussss <don.sp4r@gmail.com>
2023-10-25 01:53:32 +0300

    0x13 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	if (index == 0 && head == NULL)
+	if (head == NULL)
 		return (NULL);
 	else if (index == 0 && head != NULL)
 		return (head);
