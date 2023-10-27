38d4891db696d26688178bafe39f56525f9d340f
Donatussss <don.sp4r@gmail.com>
2023-10-27 03:35:30 +0300

    0x14 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,11 +2,12 @@
 
 /**
  * get_endianness - a function that checks the endianness
- * Returns: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian
  */
 
 int get_endianness(void)
 {
 	unsigned int m = 1;
+
 	return ((int)*((char *) &m));
 }
