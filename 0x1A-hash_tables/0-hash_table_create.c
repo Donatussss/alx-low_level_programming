06e91907ebf600f1a011741a92e493930dba53ec
Donatussss <don.sp4r@gmail.com>
2023-12-22 18:34:35 +0300

    0x1A 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
-	hash_node_t **arr = malloc(sizeof(hash_node_t) * size);
+	hash_node_t **arr = malloc(sizeof(hash_node_t *) * size);
 	unsigned int fr = 0;
 
 	if (new_table == NULL)
